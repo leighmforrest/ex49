613b6b1fdc451f26ff48846ddd618775da75baca
Leigh Michael Forrest <leigh@catbirdseat.io>
2024-11-06 18:40:06 -0500

    filtered_input coded and tested

--- test/test_filtered_input.py
+++ test/test_filtered_input.py
@@ -26,15 +26,12 @@ def test_filtered_input_invalid_input_choice(monkeypatch):
 
 
 def test_filtered_input_invalid_input_choice_error_message(monkeypatch, capsys):
-    # Simulate user input: first "green" (invalid), then "2" (valid)
     inputs = iter(["green", "2"])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     
     allowed_values = (1, 2, 3, 4, 5)
     filtered_input(allowed_values, int)
     
-    # Capture output after the function has run
     captured = capsys.readouterr()
     
-    # Check that the function printed the correct error message
     assert "Invalid input. Try again." in captured.out
