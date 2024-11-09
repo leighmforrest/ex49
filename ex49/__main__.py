7ce1a0114af9d927cb84a6131c8c622948c920d4
Leigh Michael Forrest <leigh@catbirdseat.io>
2024-11-09 06:56:59 -0500

    player initialized in main script

--- ex49/__main__.py
+++ ex49/__main__.py
@@ -1,6 +1,23 @@
-from filtered_input import filtered_input
+from ex49.character import Player
+
+
+def init_player():
+    while True:
+        print("Please enter your player's name.")
+        player_name = input("> ")
+        
+        if player_name:
+            player = Player(player_name)
+            return player
+        else: continue
+
 
 if __name__ == "__main__":
-    choices = ["one", "two"]
-    choice = filtered_input(choices)
-    print(f"Your choice is {choice}.")
+    print("Welcome to Monster Maze")
+
+    from ex49.character import Player
+
+if __name__ == "__main__":
+    print("Welcome to Monster Maze")
+    player = init_player()
+    print(player)
